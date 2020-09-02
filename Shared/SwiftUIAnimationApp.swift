36fa5e91aaaab0836a32461e39f92e609d120df1
ajpicard913 <Contact@saucedesigns.io>
2020-09-02 13:55:23 -0400

    Initial Commit

---
+++ Shared/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  Shared
+//
+//  Created by AJ Picard on 9/2/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
